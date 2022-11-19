8bceb888807d615f1486051d9c72847734ce5f1f
Jenny Belanger <jbelangr@gmail.com>
2022-11-19 17:42:29 -0500

    Generate minified files during setup

--- setup.sh
+++ setup.sh
@@ -17,40 +17,17 @@ printf "[3] Minimal (no theme)\n"
 read -p "Enter 1, 2, or 3: " theme
 if [[ "${theme}" == "1" ]]; then
 	rm scss/light.scss
-	rm public/light.html
-	rm public/css/light.min.css
-
 	rm scss/minimal.scss
 	rm public/minimal.html
-	rm public/css/minimal.min.css
-
-	mv public/dark.html public/index.html
 elif [[ "${theme}" == "2" ]]; then
 	rm scss/dark.scss
-	rm public/dark.html
-	rm public/css/dark.min.css
-
 	rm scss/minimal.scss
 	rm public/minimal.html
-	rm public/css/minimal.min.css
-
-	mv public/light.html public/index.html
-
-	sed --in-place "s|dark.min.css|light.min.css|" public/403.html
-	sed --in-place "s|dark.min.css|light.min.css|" public/404.html
 elif [[ "${theme}" == "3" ]]; then
 	rm scss/light.scss
-	rm public/light.html
-	rm public/css/light.min.css
-
 	rm scss/dark.scss
-	rm public/dark.html
-	rm public/css/dark.min.css
-
+	rm public/index.html
 	mv public/minimal.html public/index.html
-
-	sed --in-place "s|dark.min.css|minimal.min.css|" public/403.html
-	sed --in-place "s|dark.min.css|minimal.min.css|" public/404.html
 fi
 sed --in-place "s|Robroy Photo Gallery Demo|${project_title}|" public/index.html
 
@@ -59,8 +36,14 @@ sed --in-place "s|/path/to/robroy/|${project_path}|" public/.htaccess
 cp .env.example .env
 sed --in-place "s|/path/to/robroy/|${project_path}|" .env
 
+mkdir public/css
 mkdir public/images
+mkdir public/js
 mkdir public/json
+
+yarn install
+yarn build
+cp node_modules/@jlbelanger/robroy/dist/js/robroy.min.js public/js
 
 read -p "Enter the username you want to use to login: " username
 htpasswd -c .htpasswd "${username}"
